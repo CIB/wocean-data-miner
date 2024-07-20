237d0d7d19c5c2eff258237ac3f91e514e2c70ef
Christian Bielert <christian.bielert@horsemedia.com>
2024-07-20 21:27:53 +0200

    Improve cleanup script

--- src/data-extraction/clean.ts
+++ src/data-extraction/clean.ts
@@ -1,4 +1,7 @@
 /** Clean up a string that was extracted from RPGMaker */
 export function clean(str: string): string {
-  return str.replace(/\\[cC]\[\d+\]/g, "").replace(/\\[nN]\[\d+\]/g, "\n\n");
+  return str
+    .replace(/\\[cC]\[\d+\]/g, "")
+    .replace(/\\[nN]\[\d+\]/g, "\n\n")
+    .replace("\r\n", "\n");
 }
